166897846f37cb5ee28ace3d2b37865f7f4ed9a8
adina778899 <adinamemetea@yahoo.com>
2025-01-31 14:46:02 -0600

    Initial Commit

---
+++ playing sound lesson13/playing_sound_lesson13App.swift
@@ -0,0 +1,17 @@
+//
+//  playing_sound_lesson13App.swift
+//  playing sound lesson13
+//
+//  Created by natural health on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct playing_sound_lesson13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
